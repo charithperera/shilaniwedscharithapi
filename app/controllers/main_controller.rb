ffa22e657f565b0da793f0d7df880b70746fc3e1
Charith Perera <charith.p@gmail.com>
2017-01-29 19:40:57 +1100

    RSVP Submission

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,43 @@
 class MainController < ApplicationController
   def get_invitation
-    binding.pry
-    render json: {
-      'result': 'success'
-    }
+    response = {}
+    invitation = Invitation.where(:pin => params[:pin].to_i).first;
+
+    if invitation.nil?
+      response[:result] = "Not Found"
+    else
+      guests = invitation.guests
+      response[:result] = "Found"
+      response[:guests] = []
+      guests.each do |guest|
+        guest_info = {}
+        guest_info[:guest_id] = guest.id
+        guest_info[:first_name] = guest.first_name
+        guest_info[:last_name] = guest.last_name
+        guest_info[:rsvp_id] = guest.rsvp.id
+        guest_info[:attend] = guest.rsvp.attend
+        guest_info[:diet] = guest.rsvp.diet
+        response[:guests] << guest_info
+      end
+    end
+
+    render json: response
+  end
+
+  def submit_rsvp
+    attending = params.select { |key| key.include? "attending" }
+    diet = params.select { |key| key.include? "diet" }
+
+    attending.each do |key, value|
+      rsvp = Rsvp.where(:guest_id => +key[0]).first
+      rsvp.attend = value
+      rsvp.save
+    end
+
+    diet.each do |key, value|
+      rsvp = Rsvp.where(:guest_id => +key[0]).first
+      rsvp.diet = value
+      rsvp.save
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/getinvitation", to: "main#get_invitation"
+  post "/submitrsvp", to: "main#submit_rsvp"
 end
